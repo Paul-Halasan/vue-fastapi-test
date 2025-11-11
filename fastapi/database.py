ebca252c301df97b1fc5e98b799bfb10388df178
PaulHalasan <halasanpaul03@gmail.com>
2025-11-12 01:35:53 +0800

    try

--- fastapi/database.py
+++ fastapi/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # palitan mo yung password kung iba sa 'root'
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345@127.0.0.1:3306/pricing_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345@127.0.0.1/pricing_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
